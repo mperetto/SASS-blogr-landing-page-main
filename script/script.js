002b6436838818206a828b81b9809ca060fe98ef
Marco Peretto <48920479+mperetto@users.noreply.github.com>
2021-07-10 12:06:47 +0200

    Implemented header
    set:
     - header content.
     - navbar-nav style.
     - responsive menu.
     - dropdown menu.

--- script/script.js
+++ script/script.js
@@ -1,7 +1,17 @@
 const hamburgerMenu = document.querySelector('#hamburger-menu');
 
+let menuOpen = false
 hamburgerMenu.addEventListener('click', function() {
     const nav = document.querySelector('.navbar-nav');
+
+    if(menuOpen == false) {
+        menuOpen = true;
+        this.src = './images/icon-close.svg';
+    }
+    else {
+        menuOpen = false;
+        this.src = './images/icon-hamburger.svg';
+    }    
 
     nav.classList.toggle('navbar-nav--active');
 })
